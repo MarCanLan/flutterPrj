9e58fc574bd1b93277da604b948373df5619cf27
MartinaCanamares <martina.canamares@sarria.salesians.cat>
2026-01-01 12:45:00 +0100

    Eliminar carpeta equivocada

--- proyectosFlutter/moviesApi/lib/api/api_end_points.dart
+++
@@ -1,6 +0,0 @@
-class ApiEndPoints {
-  static const products = "products";
-  static const popularMovies = "movie/popular";
-  static const upcomingMovies = "movie/upcoming";
-  static const getGenreList = "genre/movie/list";
-}
